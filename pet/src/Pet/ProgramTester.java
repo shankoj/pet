1c00d7d0488ebc3978b0c361b7bfadfc3ec730a2
JShanko <Shankoj@csc.edu>
2022-11-17 10:22:22 -0600

    Create .gitignore

--- pet/src/Pet/ProgramTester.java
+++ pet/src/Pet/ProgramTester.java
@@ -29,11 +29,15 @@ public class ProgramTester{
             }else if(option==2){
                 addMorePets(scan);
             }else if(option==3){
+                searchByPetName(scan);
+            }else if(option==4){
+                searchByPetAge(scan);
+            }else if(option==5){
                 System.out.println("Goodbye!");
             }else{
                 System.out.println("Invalid choice!");
             }
-            } while (option != 3);
+            } while (option != 5);
 
                 scan.close();
         }
@@ -81,14 +85,68 @@ public class ProgramTester{
             } while (!petString.equalsIgnoreCase("done"));
                 System.out.println(count + " pets added.");
         }
-       
+       /**
+        * Searching  pet by  name.
+        * @param scan the scan
+        */
+        private static void searchByPetName(Scanner scan) {
+
+            System.out.print("Enter name to search: ");
+            String name = scan.nextLine();
+
+            System.out.println("+---------------------------------------+");
+            System.out.printf("|%5s%5s%10s%10s%5s%5s\n", "ID", "|", "NAME", "|", "AGE", "|");
+            System.out.println("+---------------------------------------+");
+            int i = 0;
+            for (Pets pet : pets) {
+
+                if (pet.getName().equalsIgnoreCase(name)) {
+
+                    System.out.printf("|%5d%5s", i, pet.toString());
+                    i++;
+                }
+            }
+            System.out.println("+---------------------------------------+");
+            System.out.println((i) + "rows in set.");
+
+        }
+
+        /**
+        * Searching pet by age.
+        *
+        * @param scan the scan
+        */
+        private static void searchByPetAge(Scanner scan) {
+
+            System.out.print("Enter age to search: ");
+            int age = scan.nextInt();
+            scan.nextLine();
+            System.out.println("+---------------------------------------+");
+            System.out.printf("|%5s%5s%10s%10s%5s%5s\n", "ID", "|", "NAME", "|", "AGE", "|");
+            System.out.println("+---------------------------------------+");
+            int i = 0;
+            for (Pets pet : pets) {
+
+                if (pet.getAge() == age) {
+
+                    System.out.printf("|%5d%5s", i, pet.toString());
+                    i++;
+                }
+            }
+            System.out.println("+---------------------------------------+");
+            System.out.println((i) + "rows in set.");
+
+        }
+
         
         public static void menu() {
 
                 System.out.println("What would like to do?");
                 System.out.println("1) View all pets.");
                 System.out.println("2) Add more pets.");
-                System.out.println("3) Exit program");
+                System.out.println("3) Search pets by name.");
+                System.out.println("4) Search pets by age.");
+                System.out.println("5) Exit program");
         }
 }
 
